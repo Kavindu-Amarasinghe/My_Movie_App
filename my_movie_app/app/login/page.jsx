de5630ac79abd95510c10871cb74810bd7fb353b
Kavindu Amarasinghe <amarasinghakavindu09@gmail.com>
2025-04-18 09:09:24 +0530

    initial add

---
+++ my_movie_app/app/login/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="w-full justify-center items-center min-h-screen bg-yellow-200 text-black text-center flex flex-col">
+      <div className="bg-green-300 p-4 rounded-2xl font-bold">Login Page</div>
+    </div>
+  );
+}
